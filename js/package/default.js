75beaeef43af3d777a6f3faad0a434530a755aaa
sukonzer <sukonzer@gmail.com>
2014-11-03 16:51:37 +0800

    Update default.js
    1.优化代码
    2.添加各页面的代码是否允许判断

--- js/package/default.js
+++ js/package/default.js
@@ -1,11 +1,8 @@
 /**
-* Created with JetBrains WebStorm.
 * User: jVan
-* Date: 13-7-14
-* Time: 上午9:13
 * Description: 全局js
 */
-;(function(){
+;(function($){
 var $bd = $('body'),
 	$doc = $(document),
 	winWd = $doc.width(),
@@ -15,128 +12,152 @@ var $bd = $('body'),
 	isScroll = true,//滚屏默认开启
 	fullIndex = 0;//全屏大图索引
 
+//如果是ie6直接给出不支持提示
+if(ol.browser.msie && ol.browser.version === '6.0'){
+	$bd.empty();
+	var fragment = '<div class="ie6-cover"><table>' +
+				'<tr><td>我們不支持落後的IE6瀏覽器，請更新您的瀏覽器後再訪問，謝謝！</td></tr>' +
+				'</table></div>';
+	$bd.append(fragment);
+}
+//=================================================
+// index首页
+//=================================================
+if($('.knight-box').length >0){
+	setTimeout(function(){
+		location.href="home.html";
+	},2000);
+}
 //=================================================
 // home菜单
 //=================================================
-$doc.on('click','.main-menu li span',function(){
-	var $this=$(this);
-	var $li=$this.next().find('li');
-	if(!$this.hasClass('extend')){
-		$this.addClass('extend');
-		var random = Math.random()*1000;
-		var ceil = Math.ceil(random);
-		$.each($li,function(i){
-			ceil--;
-			$li.eq(i).css({'zIndex': ceil});
-		});
-		var n = 0;
-		var amimate = function($obj){
-			$obj.stop().animate({
-				marginTop: 4
-			},800,function(){
-				n++;
-				if($obj.next().size()){
-					amimate($obj.next());
-				}
+if($('.main-menu').length >0){
+	$doc.on('click','.main-menu li span',function(){
+		var $this=$(this);
+		var $li=$this.next().find('li');
+		if(!$this.hasClass('extend')){
+			$this.addClass('extend');
+			var random = Math.random()*1000;
+			var ceil = Math.ceil(random);
+			$.each($li,function(i){
+				ceil--;
+				$li.eq(i).css({'zIndex': ceil});
+			});
+			var n = 0;
+			var amimate = function($obj){
+				$obj.stop().animate({
+					marginTop: 4
+				},800,function(){
+					n++;
+					if($obj.next().size()){
+						amimate($obj.next());
+					}
+				})
+			}
+			amimate($li.eq(0));
+		}else{
+			$li.stop().animate({marginTop:-30},1000,function(){
+				$this.removeClass('extend');
 			})
 		}
-		amimate($li.eq(0));
-	}else{
-		$li.stop().animate({marginTop:-30},1000,function(){
-			$this.removeClass('extend');
-		})
-	}
-});
+	});
+}
 //=================================================
-// show case
+// 秀色(show case)
 //=================================================
-//无缝滚动
-var marquee = $('#marquee'),//滚到层对象
-	marqTop = marquee.parent().offset().top,
-	marqHt = marquee.height(),
-	original = $("#original"),//原始图对象
-	clone = $("#clone");//复制对象
-marquee.css('width',winWd +'px');
+//当滚动对象存在才绑定相关事件
+if($('#marquee').length >0){
+	//无缝滚动
+	var marquee = $('#marquee'),//滚到层对象
+		marqTop = marquee.parent().offset().top,
+		marqHt = marquee.height(),
+		original = $("#original"),//原始图对象
+		clone = $("#clone");//复制对象
+	marquee.css('width',winWd +'px');
 
-//如果原始图的总宽度小于窗体宽度，不滚屏
-if(original.find('img').length*264 < winWd){
-	isScroll = false;
-}
-//绑定滚屏
-function bindScroll(isScroll){
-	if(isScroll){
-		$doc.on('mousemove','html',decision);
-		$doc.on('mouseenter','#marquee',function(){
-			scrollTimer && clearInterval(scrollTimer);
-			$doc.off('mousemove','html',decision);
-		});
-		$doc.on('mouseleave','#marquee',function(){
+	//如果原始图的总宽度小于窗体宽度，不滚屏
+	if(original.find('img').length*264 < winWd){
+		isScroll = false;
+	}
+	//绑定滚屏
+	function bindScroll(isScroll){
+		if(isScroll){
 			$doc.on('mousemove','html',decision);
-		});
+			$doc.on('mouseenter','#marquee',function(){
+				scrollTimer && clearInterval(scrollTimer);
+				$doc.off('mousemove','html',decision);
+			});
+			$doc.on('mouseleave','#marquee',function(){
+				$doc.on('mousemove','html',decision);
+			});
+		}
 	}
-}
-bindScroll(isScroll);
-$doc.on('resize',function(){
-	//重写参数值
-	marqTop = marquee[0].parentNode.offsetTop;
-	winWd = document.documentElement.clientWidth;
-	winHt = document.documentElement.clientHeight;
-	marquee[0].style.width = winWd +'px';
-});
-//查看产品图
-$doc.on('click','.fullwidth-box a',function(){ //小样图层
-	var $this = $(this);
-	idx = $this.index();
-	if($('#maskBox').length<=0){
-		$bd.append('<div class="maskbox" id="maskBox"></div>');
-	}
-	if($('#comp').length<=0){
-		$bd.append($.tmpl.render($('#compTmpl').html(),data[idx]));
-	}
-	$('#comp').addClass('s-visible');
-	$('.brand-box .detail').hide();
-	$doc.off('mouseenter','#marquee');
-	$doc.off('mouseleave','#marquee');
-});
-//关闭小样图层
-$doc.on('click','#comp>.action>span',function(){
-	$('#comp').remove();
-	$('#maskBox').remove();
-	$('.brand-box .detail').show();
 	bindScroll(isScroll);
-});
-//打开全屏图层
-$doc.on('click','#comp>.action>i',function(){
-	$('#comp>.action>span').trigger('click');
-	if($('#fullScreen').length<=0){
-		$('body').append($.tmpl.render($('#fullTmpl').html(),data[idx]));
+	//查看产品图
+	$doc.on('click','.fullwidth-box a',function(){ //小样图层
+		var $this = $(this);
+		idx = $this.index();
+		if($('#maskBox').length<=0){
+			$bd.append('<div class="maskbox" id="maskBox"></div>');
+		}
+		if($('#comp').length<=0){
+			$bd.append($.tmpl.render($('#compTmpl').html(),data[idx]));
+		}
+		$('#comp').addClass('s-visible');
+		$('.brand-box .detail').hide();
+		$doc.off('mouseenter','#marquee');
+		$doc.off('mouseleave','#marquee');
+	});
+	//关闭小样图层
+	$doc.on('click','#comp>.action>span',function(){
+		$('#comp').remove();
+		$('#maskBox').remove();
+		$('.brand-box .detail').show();
+		bindScroll(isScroll);
+	});
+	//打开全屏图层
+	$doc.on('click','#comp>.action>i',function(){
+		$('#comp>.action>span').trigger('click');
+		if($('#fullScreen').length<=0){
+			$bd.append($.tmpl.render($('#fullTmpl').html(),data[idx]));
+		}
+		$doc.on('keyup',getKey); //绑定keyup到document
+	});
+	//关闭全屏图层
+	$doc.on('click','#fullScreen>.full-close>a',function(){
+		$('#fullScreen').remove();
+		$('.brand-box .detail').show();
+		$doc.off('keyup',getKey); //去除绑定在document上的keyup
+		//先清除定时器
+		cutoverTimer && clearTimeout(cutoverTimer);
+		//索引重置
+		fullIndex = 0;
+	});
+	//左右切换图片
+	$doc.on('click','#next',function(){ //下一张
+		cutoverTimer && clearTimeout(cutoverTimer);
+		cutoverTimer = setTimeout(function(){
+			fullIndex++;
+			cutover(fullIndex);	
+		},1000);
+	});
+	$doc.on('click','#prev',function(){ //上一张
+		cutoverTimer && clearTimeout(cutoverTimer);
+		cutoverTimer = setTimeout(function(){
+			fullIndex--;
+			cutover(fullIndex);
+		},1000);
+	});
+}
+//绑定resize事件
+$(window).on('resize',function(){
+	//重写参数值
+	if($('#marquee').length >0){
+		marqTop = marquee[0].parentNode.offsetTop;
+		marquee.css('width',winWd +'px');
 	}
-	$doc.on('keyup',getKey); //绑定keyup到document
-});
-//关闭全屏图层
-$doc.on('click','#fullScreen>.full-close>a',function(){
-	$('#fullScreen').remove();
-	$('.brand-box .detail').show();
-	$doc.off('keyup',getKey); //去除绑定在document上的keyup
-	//先清除定时器
-	cutoverTimer && clearTimeout(cutoverTimer);
-	//索引重置
-	fullIndex = 0;
-});
-//左右切换图片
-$doc.on('click','#next',function(){ //下一张
-	cutoverTimer && clearTimeout(cutoverTimer);
-	cutoverTimer = setTimeout(function(){
-		fullIndex++;
-		cutover(fullIndex);	
-	},1000);
-});
-$doc.on('click','#prev',function(){ //上一张
-	cutoverTimer && clearTimeout(cutoverTimer);
-	cutoverTimer = setTimeout(function(){
-		fullIndex--;
-		cutover(fullIndex);
-	},1000);
+	winWd = $(this).width();
+	winHt = $(this).height();
 });
 //向左滚
 function MarqueeL(speed){
@@ -241,4 +262,4 @@ function cutover(n){
 		$('#prev').css('visibility','visible');
 	}
 };
-})();
+})(jQuery);
